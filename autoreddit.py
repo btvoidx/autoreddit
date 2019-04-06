f1ce75c0d6bcddebf361ff636747f3be493e5e4e
BTVoidX <37330718+BTVoidX@users.noreply.github.com>
2019-04-06 20:14:35 +0400

    0.2.9 beta
    added retry limit

--- autoreddit.py
+++ autoreddit.py
@@ -8,13 +8,15 @@ group_id = "-180517625"
 group_id_cache = "180517625"
 album_id = "261824317"
 max_posts = 24
+max_retries = max_posts * 4
+time_between = 3600
 
 user_token="9c13eb6698fca9f01add63ed27e34b7aeea299a458393bb4075b1c1d4e32f867d599300ce9483cddc5675"
 group_token="ab18b179bca28b68b8e9cc44d3354ff958887804dace0b1b30c382690009da00f90e306e5ab15dbc16e64"
 #############################################################
 
 #   Persistent vars   #######################################
-info = {"version": "0.2.8 beta", "author": "btvoidx"}
+info = {"version": "0.2.9 beta", "author": "btvoidx"}
 headers = {
 	"User-Agent": "Python Automatic posts grabber (by /u/btvoidx)"
 }
@@ -35,9 +37,13 @@ def validateURL(url):
 		url = url + ".png"
 	return url
 
+def retried(retries):
+	retries = retries + 1
+	return retries
+
 def main(subreddit):
 	post_time = int(time.time()) + 120
-	time_adds = 3600
+	retries = 0
 
 	vk_session = vk_api.VkApi(
 		token=user_token
@@ -52,6 +58,8 @@ def main(subreddit):
 			r = requests.get("https://www.reddit.com/r/{}/top.json?sort=hot&limit={}&raw_json=1".format(subreddit, max_posts), headers=headers)
 			got_r = False
 		except:
+			if retried(retries) > max_retries:
+				return "Good job, your bot stopped working!"
 			print("Unable to grab reddit posts.")
 			time.sleep(5)
 
@@ -79,7 +87,7 @@ Image URL: {}.
 		except:
 			print("Something went wrong. Trying to continue without printing about post.")
 		message = "{}\n\n/u/{}".format(post["data"]["title"].encode("utf-8"), post["data"]["author"].encode("utf-8"))
-		post_time = post_time + time_adds
+		post_time = post_time + time_between
 
 		while got_p_u:
 			try:
@@ -88,6 +96,8 @@ Image URL: {}.
 				image = "photo" + group_id + "_" + str(image["id"])
 				got_p_u = False
 			except:
+				if retried(retries) > max_retries:
+					return "Good job, your bot stopped working!"
 				print("Unable to upload photo to VK, {}".format(post["data"]["url"]))
 				time.sleep(5)
 
@@ -96,6 +106,8 @@ Image URL: {}.
 				vk.wall.post(owner_id=group_id, message=message.decode("utf-8"), publish_date=post_time, attachments=image)
 				got_p_p = False
 			except:
+				if retried(retries) > max_retries:
+					return "Good job, your bot stopped working!"
 				print("Unable to schedule post." + message.decode("utf-8"))
 				time.sleep(5)
 
