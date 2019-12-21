05251198aed7bfdf9b4acc2b1e49284388f17771
btvoidx <37330718+BTVoidX@users.noreply.github.com>
2019-12-21 20:37:16 +0400

    d

--- main.py
+++ main.py
@@ -19,18 +19,44 @@ def random_id(): #Random number for random_id
 def loadtokens():
 	return os.environ["group-token"]
 
+def failsafe(function, contin=False, *args):
+	while contin:
+		try:
+			return function(*args)
+		except Exception as e:
+			log(f"An error occurred while executing {function}: {e}", "ERROR")
+
+def eventloop(vk_session):
+	while True:
+		try:
+			longpoll = VkBotLongPoll(vk_session, "178327076")
+			log("Connected to longpoll", "TRACE")
+		except:
+			continue
+
+		try:
+			for event in longpoll.listen():
+				events.append(event)
+		except Exception as e:
+			log(f"An error occurred: {e}", "ERROR")
+
 def main(token):
 	vk_session = vk_api.VkApi(
 		token=token
 	)
 	vk = vk_session.get_api()
 
-	#while True:
-	#	try:
-	#		pass
-	#	except Exception as e:
-	#		print(f"Shit happened: {e}")
-	log(f"{token}", "TRACE")
+	while True:
+		try:
+			for e in torem:
+				events.remove(e)
+			torem = []
+			for event in events: # Parsing through every event
+				torem.append(event)
+				print(event)
+
+		except Exception as e:
+			print(f"Shit happened: {e}")
 
 if __name__ == '__main__':
 	token = loadtokens()
